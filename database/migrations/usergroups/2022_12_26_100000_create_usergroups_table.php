7a9131641aaa291fcd3b88476447f36db5246e0d
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-11-17 12:19:23 -0300

    Made key of UserGroup unique

--- database/migrations/usergroups/2022_12_26_100000_create_usergroups_table.php
+++ database/migrations/usergroups/2022_12_26_100000_create_usergroups_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('livecontrols_user_groups', function (Blueprint $table) {
             $table->id();
+            $table->string('key')->unique();
             $table->string('name');
-            $table->string('key');
             $table->string('color')->nullable();
             $table->string('description')->nullable();
             $table->timestamps();
