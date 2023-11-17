237f4d9ec778d1e03a20ec81f48a95ecdd519cd7
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2023-11-17 12:20:16 -0300

    Added UserGroupHandler class
    This class has handy functions like add/clear/remove/exists for UserGroups

---
+++ src/Scripts/UserGroupHandler.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace LiveControls\Groups\Scripts;
+
+use Exception;
+use Illuminate\Support\Collection;
+use LiveControls\Groups\Models\UserGroup;
+
+class UserGroupHandler{
+    /**
+     * Adds a single UserGroup to the database
+     *
+     * @param string $key The key of the UserGroup to be used in traits etc.
+     * @param string|null $name The name of the UserGroup, would be uppercase first of $key if nothing is set
+     * @param string|null $color The color of the UserGroup as hex
+     * @param string|null $description The description of the UserGroup
+     * @return UserGroup|null
+     */
+    public static function add(string $key, ?string $name = null, ?string $color = null, ?string $description = null): UserGroup|null
+    {
+        return UserGroup::create([
+            'key' => $key,
+            'name' => is_null($name) ? ucfirst($key) : $name,
+            'color' => $color,
+            'description' => $description
+        ]);
+    }
+
+    /**
+     * Creates a group of UserGroups
+     *
+     * @param array $groups The UserGroups as array ['key' => '', 'name' => '', 'color' => '', 'description' => '']
+     * @return Collection
+     */
+    public static function create(array $groups): Collection
+    {
+        $col = collect();
+        foreach($groups as $group)
+        {
+            $key = '';
+            $name = '';
+            $color = '';
+            $description = '';
+            if(is_string($group)){
+                $key = $group;
+                $name = ucfirst($key);
+                $color = null;
+                $description = null;
+            }else{
+                $key = $group["key"];
+                $name = array_key_exists("name", $group) ? $group["name"] : ucfirst($key);
+                $color = array_key_exists("color", $group) ? $group["color"] : null;
+                $description = array_key_exists("description", $group) ? $group["description"] : null;
+            }
+            $newGroup = UserGroup::create([
+                'key' => $key,
+                'name' => $name,
+                'color' => $color,
+                'description' => $description
+            ]);
+
+            $col->add($newGroup);
+        }
+        return $col;
+    }
+
+    public static function edit(string $key, ?string $name = null, ?string $color = null, ?string $description = null)
+    {
+        if(!static::exists($key)){
+            throw new Exception("UserGroup with key '".$key."' does not exist!");
+        }
+        static::remove($key);
+        static::add($key, $name, $color, $description);
+    }
+
+    /**
+     * Removes a single UserGroup
+     *
+     * @param string $key The key of the usergroup
+     * @return boolean
+     */
+    public static function remove(string $key): bool
+    {
+        return UserGroup::where('key', '=', $key)->delete();
+    }
+
+    /**
+     * Truncates the whole UserGroup table.
+     * DANGEROUS!
+     *
+     * @return void
+     */
+    public static function clear()
+    {
+        return UserGroup::truncate();
+    }
+
+    /**
+     * Checks if a UserGroup with a certain key exists
+     *
+     * @param string $key
+     * @return boolean
+     */
+    public static function exists(string $key): bool
+    {
+        return UserGroup::where('key', '=', $key)->exists();
+    }
+}
